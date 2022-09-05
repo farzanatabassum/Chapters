eacf5558173782dd80aaeae3ae6d8d07edf4970d
farzanatabassum <farzanatabassum531@gmail.com>
2022-09-05 13:04:16 +0600

    nav, footer added to every page

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <>
+ <NavBar />
+  <Component {...pageProps} />
+  <Footer />
+
+  </>
+}
+
+export default MyApp
---
+++ pages/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_tJIMgdXfHznOrAeZFddXxe0nDOjacyQ",
+  authDomain: "chapters-5acec.firebaseapp.com",
+  projectId: "chapters-5acec",
+  storageBucket: "chapters-5acec.appspot.com",
+  messagingSenderId: "1089881397981",
+  appId: "1:1089881397981:web:338b277f6d996d3aa91934"
+};
+
+const app = initializeApp(firebaseConfig);

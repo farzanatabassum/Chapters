6169b932bb2ec40294df49f415f16e0f1b48b1e9
farzanatabassum <farzanatabassum531@gmail.com>
2022-09-11 11:29:27 +0600

    solved minor issues

--- components/NavBar.js
+++ components/NavBar.js
@@ -57,7 +57,7 @@ const NavBar = () => {
 
         <li   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Create a Blog</li>
 
-        <li  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign In</li>
+        <li  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign In/Sign Out</li>
 
         </ul>
       {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
